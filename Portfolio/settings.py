ba6b3def150b6f37a2cc776ca3bdeaf867df7c13
Tsav Samson <tsavsamson2014@gmail.com>
2024-08-20 16:32:56 +0100

    added railway requirements

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -112,10 +113,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static/"),
-]
+#STATICFILES_DIRS = [
+  # os.path.join(BASE_DIR, "static/"),
+#]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -135,3 +137,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
