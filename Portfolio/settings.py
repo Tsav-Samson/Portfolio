3d05522706d7a2176bde7fab1507bc3897abc8ae
Tsav Samson <tsavsamson2014@gmail.com>
2024-08-20 11:52:13 +0100

    edited the settings file

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
+# Initialize the environment
+env = environ.Env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,10 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i%0(^@4shqznd094@09eu=9d-j9)=##7!zxr_#vzv*h88xiopc'
+# Read the .env file
+env.read_env(BASE_DIR / '.env')
+
+# Access the SECRET_KEY
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
